46b66513b0a574ba82e10ece0fea3f2e4146e9d5
이규민 <83911440+keke5149@users.noreply.github.com>
2024-02-07 10:47:53 +0900

    User 통일

--- src/main/java/MeoipZi/loginMeoipZi/service/UserService.java
+++ src/main/java/MeoipZi/loginMeoipZi/service/UserService.java
@@ -38,7 +38,6 @@ public class UserService {
         User user = User.builder()
                 .username(userDto.getUsername())
                 .password(passwordEncoder.encode(userDto.getPassword()))
-                .nickname(userDto.getNickname())
                 .authorities(Collections.singleton(authority))
                 .activated(true)
                 .build();
