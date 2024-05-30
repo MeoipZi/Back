d638ea41b4dcfde3fe8524af11bde6aa31de1ded
leeyunjin <yuncom21@ewhain.net>
2024-05-30 23:45:06 +0900

    username 이 아닌 nickname으로 조회되도록 수정

--- src/main/java/meoipzi/meoipzi/comment/dto/CommentCommunityResDTO.java
+++ src/main/java/meoipzi/meoipzi/comment/dto/CommentCommunityResDTO.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import meoipzi.meoipzi.comment.domain.CommentCommunity;
+import meoipzi.meoipzi.community.domain.Community;
+import meoipzi.meoipzi.login.domain.User;
+import meoipzi.meoipzi.profile.domain.Profile;
 
 @Getter
 @Setter
@@ -13,7 +16,21 @@ public class CommentCommunityResDTO {
     private String content;
 
     public CommentCommunityResDTO(CommentCommunity commentCommunity){
-        this.username = commentCommunity.getUsername();
+        //this.username = commentCommunity.getUsername();
+        //this.username = commentCommunity.getUser().getProfile().getNickname();
+        //this.username = (!community.isAnonymous())? community.getUser().getProfile().getNickname() : "익명";
+        User user = commentCommunity.getUser();
+        if (user == null) {
+            this.username = "익명";
+        } else {
+            Profile profile = user.getProfile();
+            if (profile == null || profile.getNickname() == null || profile.getNickname().isEmpty()) {
+                this.username = "익명";
+            } else {
+                this.username = profile.getNickname();
+            }
+        }
+
         this.content = commentCommunity.getContent();
     }
 }
